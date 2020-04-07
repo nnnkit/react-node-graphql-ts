746146f2e13b9169174531d5c01e18ad351486a7
nnnkit <iankit17@gmail.com>
2020-04-07 22:47:06 +0530

    add boilerplate for react app

---
+++ client/src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>Hello React + Node</div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
