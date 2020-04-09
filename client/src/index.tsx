8784719b77ceadef1cc9d790ceb2c3ca80aa7a9a
nnnkit <iankit17@gmail.com>
2020-04-09 21:52:16 +0530

    add reouting

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { Listings } from "./section/Listings";
 import "./styles/index.css";
+import { Home, Host, Listing, NotFound } from "./section";
 const client = new ApolloClient({
   uri: "/graphql",
 });
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/host" component={Host} />
+        <Route exact path="/listing/:id" component={Listing} />
+        <Route exact path="/listings/:location?" component={Listings} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <Listings />
+      <App />
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
